ab3505b185be628fb4d53456773c6963ce668e53
Erick Christian <erickchristian48@gmail.com>
2020-12-09 19:32:31 +0700

    change: web title

--- next-seo.config.ts
+++ next-seo.config.ts
@@ -1,12 +1,12 @@
 import { DefaultSeoProps } from 'next-seo'
 
 export const defaultSeoConfig: DefaultSeoProps = {
-  title: 'Kota Bogor',
-  titleTemplate: '%s | Punten Bogor',
+  title: 'Bogor',
+  titleTemplate: '%s | Bogor',
   description: 'Indahnya wisata di Bogor',
   openGraph: {
     description: 'Indahnya wisata di Bogor',
-    title: 'Punten Bogor',
-    site_name: 'Punten Bogor',
+    title: 'Bogor',
+    site_name: 'Bogor',
   },
 }
